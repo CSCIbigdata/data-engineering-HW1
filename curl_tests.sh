813c14fdc7e60ee264322a7ffed0637ab5aacda5
Alexander Tsankov <antsankov@gmail.com>
2015-01-28 22:06:53 -0700

    Got a working put

--- curl_tests.sh
+++ curl_tests.sh
@@ -1,4 +1,8 @@
 Requests: 
 
 #Post a new recipe
-curl -i -H "Content-Type: application/json" -X POST -d '{"title":"shit","description":"garbage","stars":"3"}' http://localhost:5000/api/1.0/recipes
+curl -i -H "Content-Type: application/json" -X POST -d '{"title":"fish and chips","description":"garbage","stars":"3"}' http://localhost:5000/api/1.0/recipes
+
+#Modify a recipe with a put:
+
+curl -i -H "Content-Type: application/json" -X PUT -d '{"stars":10}' http://localhost:5000/todo/api/v1.0/recipes/1
--- rest.py
+++ rest.py
@@ -3,6 +3,7 @@ from flask import Flask,jsonify,request
 
 app = Flask(__name__)
 
+#Just to initialize our sample database
 recipes = [
 	
 	{
@@ -20,19 +21,21 @@ recipes = [
 
 ]
 
-
+#this GET returns all of the recipes
 @app.route('/api/1.0/recipes', methods=['GET'])
 def get_recipes():
     return jsonify({'recipes':recipes})
 
+#this GET returns a specific recipe based on the ID 
 @app.route('/api/1.0/recipes/<int:recipe_id>',methods=['GET'])
 def get_recipe(recipe_id):
 	recipe = [recipe for recipe in recipes if recipe['id'] == recipe_id]
-	return jsonify({'recipe':recipe[0]}
+	return jsonify({'recipe':recipe[0]})
 
+#this is a POST request used to create a recipe see the curl_tests to see how to make a new object
 @app.route('/api/1.0/recipes',methods=['POST'])
 def create_recipe():
-	if not request.json or not title in request.json:
+	if not request.json or not 'title' in request.json:
 		return "BAD POST"
 
 	recipe = {
@@ -44,6 +47,15 @@ def create_recipe():
 
 	recipes.append(recipe)
 	return jsonify({'recipe': recipe}),201
+
+# This is PUT request to modify an object 
+@app.route('/api/1.0/recipes/<int:recipe_id>',methods=['PUT'])
+def modify_recipe(recipe_id):
+	recipe = [recipe for recipe in recipes if recipe['id'] == recipe_id]
+	recipe[0]['title'] = request.json.get('title', recipe[0]['title'])
+	recipe[0]['description'] = request.json.get('description', recipe[0]['description'])
+	recipe[0]['stars'] = request.json.get('stars', recipe[0]['stars'])
+	return jsonify({'recip': recipe[0]})
 
 
 if __name__ == '__main__':
